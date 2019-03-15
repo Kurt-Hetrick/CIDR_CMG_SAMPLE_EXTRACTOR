e2b0d248325d2cc69c2a634b92d702a46b134cee
Kurt Hetrick <khetric1@jhmi.edu>
2019-03-15 11:41:17 -0400

    updated this to remove unused alternate alleles, star alleles and deal with the compressed vcf input

--- scripts/A.01_EXTRACT_MENDEL_SAMPLE.sh
+++ scripts/A.01_EXTRACT_MENDEL_SAMPLE.sh
@@ -17,6 +17,7 @@ FAMILY=$2
 IN_VCF=$3 # full path to the vcf file
 
 mkdir -p $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/$FAMILY"_ANNOVAR"
+mkdir -p $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/TEMP
 
 # Extract out sample, remove non-passing, non-variant
 
@@ -28,5 +29,18 @@ $JAVA_1_8/java -jar $GATK_DIR/GenomeAnalysisTK.jar \
 -ef \
 -env \
 --keepOriginalAC \
+--removeUnusedAlternates \
 --variant $IN_VCF \
--o $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/$FAMILY"_ANNOVAR"/$SM_TAG"_MS_OnBait.vcf"
+-o $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/TEMP/$SM_TAG"_MS_OnBait.vcf.gz"
+
+# Now remove the records where the alternate allele is just a *
+
+	( zgrep "^#" $CORE_PATH/$PROJECT_SAMPLE/TEMP/$SM_TAG"_MS_OnBait.TEMP.vcf.gz" ; \
+		zgrep -v "^#" $CORE_PATH/$PROJECT_SAMPLE/TEMP/$SM_TAG"_MS_OnBait.TEMP.vcf.gz" | awk '$5!="*"' ) \
+	| $TABIX_DIR/bgzip -c /dev/stdin \
+	>| $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/$FAMILY"_ANNOVAR"/$SM_TAG"_MS_OnBait.vcf.gz"
+
+# index the vcf file
+
+	$TABIX_DIR/tabix -f -p vcf \
+	$CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/$FAMILY"_ANNOVAR"/$SM_TAG"_MS_OnBait.vcf.gz"
--- scripts/B.01_COPY_MENDEL_SAMPLE_TO_TEMP.sh
+++ scripts/B.01_COPY_MENDEL_SAMPLE_TO_TEMP.sh
@@ -19,5 +19,5 @@ mkdir -p $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/TEMP/$SM_TAG
 
 # Copy the vcf file into a temp directory
 
-cp -rvf $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/$FAMILY"_ANNOVAR"/$SM_TAG"_MS_OnBait.vcf" \
+zcat $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/$FAMILY"_ANNOVAR"/$SM_TAG"_MS_OnBait.vcf.gz" \
 $CORE_PATH/M_Valle_MD_SeqWholeExome_120417_1_PLAYGROUND/TEMP/$SM_TAG/$SM_TAG"_MS_OnBait.vcf"
